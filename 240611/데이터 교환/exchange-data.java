1e7f5b31bda925b54e4127e89c75dea86bd7a183
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-11 22:21:34 +0900

    Title: 데이터 교환, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240611/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int a_1 = c;
+        int b_1 = b;
+        int c_1 = a;
+        
+
+        System.out.println(a_1);
+        System.out.println(c_1);
+        System.out.println(b_1);
+    }
+}
