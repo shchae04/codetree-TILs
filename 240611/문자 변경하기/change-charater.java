494210fe9dcf8539a9aa95d92973d5de0b26147c
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-11 22:03:21 +0900

    Title: 문자 변경하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240611/문자 변경하기/change-charater.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        String a = "C";
+        String b = "T";
+
+        System.out.println(b);
+        
+    }
+}
