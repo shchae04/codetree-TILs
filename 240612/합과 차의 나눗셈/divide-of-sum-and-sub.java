c8d98329bb461b3005acbb35c7d3ace835fcbca7
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-12 09:33:02 +0900

    Title: 합과 차의 나눗셈, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240612/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        double ex = (double)(a+b)/(a-b); // 13/7
+        System.out.printf("%.2f", ex);
+    }
+}
