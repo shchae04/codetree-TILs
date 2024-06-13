cac7cbd82b44aa98d4b244646281122c9e336fe1
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-13 13:58:12 +0900

    Title: 짝수만 출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240613/짝수만 출력/output-only-even.java
+++ 240613/짝수만 출력/output-only-even.java
@@ -3,17 +3,19 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        // 변수 선언
+        int a, b;
+        int i;
 
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        i = a;
 
-        while(a <= b){
-            if(a % 2==0){
-                System.out.print(a + " ");
-                a = a + 2;
-            } else {
-                a = a + 1;
-            }
+        // 출력
+        while(i <= b) {
+            System.out.print(i + " ");
+            i += 2;
         }
     }
 }
