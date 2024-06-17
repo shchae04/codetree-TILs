27854376791f46f3db54521414e0ff433332168c
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-17 11:52:59 +0900

    Title: 순서쌍 만들기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/순서쌍 만들기/making-order-pair.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int n = (new Scanner(System.in)).nextInt();
+
+        for(int i=n; i>=1; i--){
+            for(int j=n; j>=1; j--){
+                System.out.print("(" + i + "," + j + ") ");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
