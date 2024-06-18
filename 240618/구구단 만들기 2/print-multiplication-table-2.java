b12ed2cc403d5f598b77e6b8aaa41b9bd71f5611
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-18 23:53:02 +0900

    Title: 구구단 만들기 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240618/구구단 만들기 2/print-multiplication-table-2.java
+++ 240618/구구단 만들기 2/print-multiplication-table-2.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int cnt = 2;
-        for(int i=b; i>=a-1; i--){
-            for(int j=0; j<b-a+1; j++){ //3개 j<3 012
-                System.out.print((b-j) + " * " + cnt + " = " + (b-j)*cnt +" ");
-                if(b-j > 2){
-                    System.out.print("/ ");
+        // 각 행에 대해 반복하여 출력
+        for (int i = 2; i <= 8; i += 2) {
+            // 각 열은 b부터 a까지 감소하는 순서로 출력
+            for (int j = b; j >= a; j--) {
+                System.out.print(j + " * " + i + " = " + (j * i));
+                // 맨 마지막 곱셈 결과에는 / 출력하지 않음
+                if (j > a) {
+                    System.out.print(" / ");
                 }
-                // 4-0 = 4-1 4-2
-
             }
-            cnt+=2;
             System.out.println();
         }
+
+        sc.close();
     }
 }
