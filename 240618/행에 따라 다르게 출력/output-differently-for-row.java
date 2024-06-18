d7e9c889a45fc336da0f4837027bc16a3ac3f741
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-18 22:44:18 +0900

    Title: 행에 따라 다르게 출력, Time: 227ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240618/행에 따라 다르게 출력/output-differently-for-row.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int currentNumber = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                // 홀수 줄 (0, 2, 4, ...)
+                for (int j = 0; j < n; j++) {
+                    System.out.print(currentNumber +" ");
+                    if(j != n-1) currentNumber++;
+                }
+                currentNumber+=2;
+            } else {
+                // 짝수 줄 (1, 3, 5, ...)
+                for (int j = 0; j < n; j++) {
+                    System.out.print(currentNumber +" ");
+                    if(j != n-1){
+                        currentNumber+=2;
+                    }
+                    
+                
+                
+                }
+                        currentNumber+=1;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
