6f74ee8e56b926a8f344aa2dd62a3b009ffe81db
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-18 21:08:53 +0900

    Title: 좌우로 반복하며 출력, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240618/좌우로 반복하며 출력/print-in-left-right-reverse.java
+++ 240618/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -17,4 +17,16 @@ public class Main {
             System.out.println();
         }
     }
+
+    // for(int i = 0; i < n; i++) {
+    //         for(int j = 0; j < n; j++) {
+    //             if(i % 2 == 0) {
+    //                 System.out.print(j + 1);
+    //             }
+    //             else {
+    //                 System.out.print(n - j);
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
 }
