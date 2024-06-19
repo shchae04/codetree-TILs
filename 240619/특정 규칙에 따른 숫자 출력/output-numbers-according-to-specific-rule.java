10598175126d3d394c466787ef79c240dbc914d7
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-19 10:58:28 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240619/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt =1;
+
+        for (int i = 0; i < n; i++) { //0 1 2 3
+            //공백의 갯수 숫자의 갯수 i번 만큼 공백 출력
+            for (int k = 0; k < i; k++) {
+                        System.out.print("  ");
+                    }
+            for (int j = 0; j < n-i; j++) { // 4 3 2 1 출력
+                System.out.print(cnt +" ");
+                cnt++;
+                if(cnt > 9){
+                    cnt = 1;
+                }
+            }
+            System.out.println();
+        }
+
+        // 숫자로 이루어진 삼각형을 출력합니다.
+        // for(int i = n; i >= 1; i--) {
+        //     for(int j = n; j >= 1; j--) {
+        //         if(j > i)
+        //             System.out.print("  ");
+        //         else
+        //             System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
