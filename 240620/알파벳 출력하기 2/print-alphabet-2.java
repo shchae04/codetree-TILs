e7e51cbf1285afb905a606cc56d76b224b3d2a0e
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-20 09:55:23 +0900

    Title: 알파벳 출력하기 2, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240620/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        char cChar = 'A';
+
+        for(int i=0; i<n; i++){
+            
+            for(int k=0; k<i+1 ; k++){
+                if(k != 0 )
+                System.out.print("  ");
+            }
+
+            for(int j=0; j<n-i; j++){
+                System.out.print(cChar + " ");
+                cChar++;
+                if(cChar > 'Z'){
+                    cChar = 'A';
+                }
+            }
+            System.out.println();
+        }
+    }
+}
