0c798d5d1d11a0f62843b0dae3dbba05febecd7c
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-20 15:43:43 +0900

    Title: 약수가 세 개인 수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240620/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 240620/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+     public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
 
-        int start = sc.nextInt(); // start
-        int end = sc.nextInt(); // end
+    int start = sc.nextInt(); // start
+     int end = sc.nextInt(); // end
 
-        int sum = 0;
+    int sum = 0;
 
-        for (int i = start; i <= end; i++) {
-            int k = 1;
-            int cnt = 0; // 약수의 갯수 초기화
+    for (int i = start; i <= end; i++) {
+ int k = 1;
+ int cnt = 0; // 약수의 갯수 초기화
 
-            while (k < i) { // start 가 아닐때까지만 1 6
-                if (i % k == 0) {
-                    cnt++;
-                }
-                k++;
-            }
+ while (k <= i) { // start 가 아닐때까지만 1 6
+ if (i % k == 0) {
+ cnt++;
+ }
+ k++;
+ }
 
-            if (cnt == 3) {
-                sum++;
-            }
-        }
+if (cnt == 3) { // 약수의 개수가 3개인 경우
+ sum++;
+ }
+ }
 
-        System.out.println(sum);
-        sc.close();
-    }
+ System.out.println(sum);
+ sc.close();
+}
 }
