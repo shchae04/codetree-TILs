be467e8275109ccaacad9abad948204f9b2de7c2
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-22 23:05:54 +0900

    Title: 나눗셈의 나머지, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240622/나눗셈의 나머지/remainder-of-division.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arrCount = new int[10];
+        int i;
+
+        while (a > 0) {
+            i = a % b;
+            arrCount[i]++;
+
+
+            a /= b;
+        }
+
+        int result = 0;
+        for (int count : arrCount) {
+            result += count * count;
+        }
+        System.out.println(result);
+
+        sc.close();
+    }
+}
