9c01088a940c5d968a158c1eb4de35d338b083f0
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-23 15:55:21 +0900

    Title: 특정 원소들의 합, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240623/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[4][4];
+
+        //값 입력
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //계산
+        int sum = 0;
+        for(int i=0; i<4; i++){
+            for(int j=0; j<i+1; j++){
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
