83adeb22a649854649af8445e6862a0c90ce265b
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-26 14:55:55 +0900

    Title: 공백을 기준으로 출력, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/공백을 기준으로 출력/output-based-on-space.java
+++ 240626/공백을 기준으로 출력/output-based-on-space.java
@@ -10,5 +10,14 @@ public class Main {
         String trimmedString = str.replaceAll("\\s+", "");
         String trimmedString2 = str2.replaceAll("\\s+", "");
         System.out.println(trimmedString + trimmedString2);
+
+        // 문자열을 전부 순회하며 공백을 제외한 모든 문자를 출력합니다.
+        // for(int i = 0; i < str.length(); i++)
+        //     if(str.charAt(i) != ' ')
+        //         System.out.print(str.charAt(i));
+        
+        // for(int i = 0; i < str2.length(); i++)
+        //     if(str2.charAt(i) != ' ')
+        //         System.out.print(str2.charAt(i));
     }
 }
