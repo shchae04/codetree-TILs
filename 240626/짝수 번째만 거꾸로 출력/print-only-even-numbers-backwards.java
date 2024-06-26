f0dadd8baa047f77c18b795773301990e7627b00
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-26 22:37:21 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
+++ 240626/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine(); // 문자열 입력 받기
-        
+
         StringBuilder result = new StringBuilder();
-        
+
         // 짝수 번째 문자만 추출하여 역순으로 result에 저장
-        for (int i = input.length() - 1; i >= 0; i -= 2) {
-            result.append(input.charAt(i));
+        for (int i = 0; i < input.length(); i++) {
+            if (i % 2 != 0) { // 짝수 번째 인덱스인 경우만 처리
+                result.insert(0, input.charAt(i));
+            }
         }
-        
-        System.out.println(result.toString());
+
+        System.out.println(result.toString()); // 출력
     }
 }
