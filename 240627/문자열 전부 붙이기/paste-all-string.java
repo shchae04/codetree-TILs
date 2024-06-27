e893e3669b268b15a4dcae70c194f53d5241e432
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-27 20:25:55 +0900

    Title: 문자열 전부 붙이기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240627/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String[] arr = new String[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.next();
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
