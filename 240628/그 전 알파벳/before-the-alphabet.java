078d57b21e664ac53bcfd4635f83af3ce91345cd
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-28 10:18:57 +0900

    Title: 그 전 알파벳, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240628/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        // 알파벳 소문자를 입력받습니다.
+        char x = sc.next().charAt(0);
+
+        // 다음 알파벳을 출력합니다.
+        if(x == 'a')
+            System.out.print("z");
+        else
+            System.out.print((char)(x - 1));
+        
+    }
+}
