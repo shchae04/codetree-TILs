1baa07c6df785db97d7f83145ca14884e1977e06
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-06-28 10:14:22 +0900

    Title: 그 다음 알파벳, Time: 110ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240628/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = (int)sc.next().charAt(0); //숫자로 변환
+
+
+        if(a == (int)'z'){
+            System.out.println('a');
+        }
+        System.out.println((char)(a + 1));
+        
+        
+
+        
+    }
+}
