e21de6839ab782e175fc4d1d197f3915ad32be96
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-02 17:16:36 +0900

    Title: 반복 출력하기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240702/반복 출력하기/repeated-output.java
+++ 240702/반복 출력하기/repeated-output.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void printNLines(int n) {
+    // n개의 줄에 걸쳐 특정 문자열을 출력하는 함수입니다.
+    public static void printString(int n) {
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < 5; j++){
-                System.out.print(j+1);
-            }
-            System.out.print("^&*()_");
-            System.out.println();
+            System.out.println("12345^&*()_");
         }
     }
     public static void main(String[] args) {
+        // 변수 선언 및 입력:
         Scanner sc = new Scanner(System.in);
         int rowNum = sc.nextInt();
-        printNLines(rowNum);
+        printString(rowNum);
     }
 }
