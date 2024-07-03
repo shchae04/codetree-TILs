04e89ba1874413ea93e1e87c9f169b2c54a79692
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-03 09:19:59 +0900

    Title: 정수의 최솟값, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240703/정수의 최솟값/minimum-value-of-an-integer.java
+++ 240703/정수의 최솟값/minimum-value-of-an-integer.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void calc(int a, int b, int c) {
         int result = 0;
-        if(a < b && a < c)
+        if(a <= b && a <= c)
         result = a;
-        if(b < a && b < c)
+        if(b <= a && b <= c)
         result = b;
-        if(c < a && c < b)
+        if(c <= a && c <= b)
         result = c;
 
         System.out.println(result);
