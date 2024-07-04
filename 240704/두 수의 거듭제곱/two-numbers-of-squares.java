00df3d9ba07e32a200071f224a7b82b1f4bb0e45
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-04 14:07:05 +0900

    Title: 두 수의 거듭제곱, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240704/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(calc(a,b));
+    }
+
+    public static int calc(int a, int b){
+        int result = 1;
+        for(int i=0; i<b; i++){
+            result *= a;
+        }
+
+        return result;
+    }
+}
