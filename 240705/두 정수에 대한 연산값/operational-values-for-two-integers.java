ee0845480580e29ab6c3d3860112aba392e2cad3
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-05 09:51:49 +0900

    Title: 두 정수에 대한 연산값, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240705/두 정수에 대한 연산값/operational-values-for-two-integers.java
+++ 240705/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -1,24 +1,43 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        Integer A = a;
-        Integer B = b;
-        calc(A,B);
+class IntWrapper {
+    int value;
+
+    public IntWrapper(int value) {
+        this.value = value;
+    }
+}
+
+public class Main {    
+    // 문제에서 설명한 대로 a와 b를 변경합니다.
+    // call by reference를 구현합니다.
+    public static void changeNumber(IntWrapper a, IntWrapper b) {
+        if(a.value > b.value) {
+            b.value *= 2;
+            a.value += 25;
+        }
+        else {
+            a.value *= 2;
+            b.value += 25;
+        }
+    
+        return;
     }
 
-    public static void calc(Integer a, Integer b){
-        if(a>b){
-            a= a + 25;
-            b = b * 2;
-        } else {
-            b = b +25;
-            a = a *2;
-        }
-        System.out.println(a + " " + b);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        IntWrapper aWrapper = new IntWrapper(a);
+        IntWrapper bWrapper = new IntWrapper(b);
+
+        changeNumber(aWrapper, bWrapper);
+
+        a = aWrapper.value;
+        b = bWrapper.value;
+
+        System.out.print(a + " " + b);
     }
 }
