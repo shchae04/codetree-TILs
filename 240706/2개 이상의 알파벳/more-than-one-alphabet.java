9e096c6010eeb22bf7dda71cce4b99ab34349d63
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-06 16:05:10 +0900

    Title: 2개 이상의 알파벳, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240706/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 240706/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -25,4 +25,16 @@ public class Main {
     
     return "No";
 }
+
+// 문자열에 알파벳이 2개 이상인지 여부를 반환합니다.
+    public static boolean isMoreTwoalp(String str) {
+        // 하나라도 서로 다른 알파벳이 있다면
+        // 그 문자열은 알파벳이 2개 이상입니다.
+        int len = str.length();
+        for(int i = 0; i < len; i++)
+            if(str.charAt(i) != str.charAt(0))
+                return true;
+    
+        return false;
+    }
 }
