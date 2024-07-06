02c62ca86d1db701c5f268b304c4db589d18bd15
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-06 17:15:07 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240706/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
+++ 240706/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -1,28 +1,57 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();  
-        String in = sc.nextLine();   
 
-        int result = calc(str,in);
-        System.out.println(result);
-    }
+    public static String text, pattern;
 
-    public static int calc(String str, String in){
-        for(int i=0; i <= str.length() - in.length(); i++){  
-            boolean flag = true;
-            for(int j=0; j<in.length(); j++){  
-                if(str.charAt(i+j) != in.charAt(j)){
-                    flag = false;
-                    break;
-                }
-            }
+    public static boolean isSubStr(int startIdx){
+        int n = text.length();
+        int m = pattern.length();
 
-            if(flag){
-                return i;
+        // startIdx + m -1 >= n 이면 text의 크기를 초과
+        if(startIdx + m -1 >= n){
+            return false;
+        }
+
+        for(int j=0; j<m; j++){
+            if(text.charAt(startIdx + j) != pattern.charAt(j)){
+                return false;
             }
         }
+        return true;
+    }
+
+    public static int findIndex(){
+        int n = text.length();
+        for(int i=0; i<n; i++){
+            if(isSubStr(i))
+                return i;
+        }
+
         return -1;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        text = sc.nextLine();  
+        pattern = sc.nextLine();   
+
+        System.out.println(findIndex());
+    }
+
+    // public static int calc(String str, String in){
+    //     for(int i=0; i <= str.length() - in.length(); i++){  
+    //         boolean flag = true;
+    //         for(int j=0; j<in.length(); j++){  
+    //             if(str.charAt(i+j) != in.charAt(j)){
+    //                 flag = false;
+    //                 break;
+    //             }
+    //         }
+
+    //         if(flag){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
