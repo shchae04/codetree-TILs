d300e4c141a315bce360b5e83ba4bd7af3ccd82f
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-06 15:54:10 +0900

    Title: 절댓값으로 변경, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240706/절댓값으로 변경/find-the-absolute-value.java
+++ 240706/절댓값으로 변경/find-the-absolute-value.java
@@ -12,18 +12,15 @@ public class Main {
         }
 
         calc(arr);
+
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
     }
 
     public static void calc(int[] arr){
-        for(int i=0; i<arr.length; i++){
-
-            if(arr[i] == 0) {
-                System.out.print(arr[i] + " ");
-            } else if(arr[i] < 0){
-                System.out.print(Math.abs(arr[i]) + " ");
-            } else {
-                System.out.print(Math.abs(arr[i]) + " ");
-            }
-        }
+        for(int i=0; i<arr.length; i++)
+            if(arr[i]  < 0 )
+                arr[i] = -arr[i];
+        return;
     }
 }
