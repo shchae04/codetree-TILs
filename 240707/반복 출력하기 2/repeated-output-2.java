95d0301fe24d9ab7697ba8fd3260c87f829e41fd
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-07 21:23:29 +0900

    Title: 반복 출력하기 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240707/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printStar(n);
+    }
+
+    public static void printStar(int n) { 
+        if(n == 0)                        
+            return;                       
+        
+        printStar(n - 1);                 
+        System.out.println("HelloWorld");      
+    }
+}
