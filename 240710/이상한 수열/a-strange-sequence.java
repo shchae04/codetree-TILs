24e743341363ec8e23ac743168243e2ab0a96274
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-10 13:10:56 +0900

    Title: 이상한 수열, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240710/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+
+        System.out.println(F(n));
+    }
+
+    public static int F(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        return F(n/3) + F(n-1);
+    }
+}
