76b1d0037e2ff08899e00beab781558bf135a78b
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-11 16:45:01 +0900

    Title: 단어 정렬, Time: 174ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240711/단어 정렬/sorting-words.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] words = new String[n];
+        for(int i=0; i<words.length; i++){
+            words[i] = sc.next();
+        }
+        Arrays.sort(words);
+
+        for(int i = 0; i < n; i++)
+            System.out.println(words[i]); 
+    }
+}
