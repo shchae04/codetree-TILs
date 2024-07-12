3ab3371a98ee92a9330af23cf89af5d0c655f5e4
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-12 10:31:29 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 250ms, Memory: 15MB, Status: Passed - CodeTree

--- 240712/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
+++ 240712/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -1,30 +1,32 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String str1 = sc.next();
         String str2 = sc.next();
 
-        //순서를 바꾸어 동일한 단어 가능한지.
-        //정렬 했을때 같아야 한다. 
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
+        // 각 문자의 출현 빈도를 저장할 배열
+        int[] freq1 = new int[128]; // ASCII 문자를 고려
+        int[] freq2 = new int[128];
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        for(int i=0; i<arr1.length; i++){
-            if(arr1[i] != arr2[i]){
-                System.out.println("No");
-                return;
-            } 
+        // 각 문자열의 문자 빈도 계산
+        for (char c : str1.toCharArray()) {
+            freq1[c]++;
+        }
+        for (char c : str2.toCharArray()) {
+            freq2[c]++;
         }
 
-        System.out.println("Yes");
+        // 빈도 비교
+        boolean canForm = true;
+        for (int i = 0; i < 128; i++) {
+            if (freq1[i] != freq2[i]) {
+                canForm = false;
+                break;
+            }
+        }
 
-
+        System.out.println(canForm ? "Yes" : "No");
     }
 }
