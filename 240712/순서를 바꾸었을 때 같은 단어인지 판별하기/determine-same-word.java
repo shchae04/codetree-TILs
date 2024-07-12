84996948b6f36e822cfec608d3567d0546defdd5
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-12 10:43:39 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 270ms, Memory: 14MB, Status: Passed - CodeTree

--- 240712/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
+++ 240712/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
+    public static final int ASCII_NUM = 128;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.next();
         String str2 = sc.next();
+        int[] count = new int[ASCII_NUM];
 
-        // 각 문자열을 정렬합니다.
-        char[] chars1 = str1.toCharArray();
-        Arrays.sort(chars1);
-        String sortedStr1 = new String(chars1);
+        // count 배열을 0으로 초기화 해줍니다.
+        for(int i = 0; i < ASCII_NUM; i++)
+            count[i] = 0;
 
-        char[] chars2 = str2.toCharArray();
-        Arrays.sort(chars2);
-        String sortedStr2 = new String(chars2); 
+        // 첫 번째 문자열을 순회하며 각 문자의 개수를 세줍니다. 
+        for(int i = 0; i < str1.length(); i++)
+            count[str1.charAt(i)]++;
 
-        if(sortedStr1.equals(sortedStr2))
-            System.out.print("Yes");
-        else
-            System.out.print("No");
+        // 두 번째 문자열을 순회하며 각 문자의 개수를 세줍니다. 
+        for(int i = 0; i < str2.length(); i++)
+            count[str2.charAt(i)]--;
+
+        // count 배열을 순회하면서 모든 문자의 개수가 동일한지를 확인합니다.
+        for(int i = 0; i < ASCII_NUM; i++)
+            if(count[i] != 0) {
+                System.out.print("No");
+                System.exit(0);
+            }
+
+        System.out.print("Yes");
     }
 }
