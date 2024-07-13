494923c8351681c3b47ffaffd8917aab674a9efc
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-14 01:51:44 +0900

    Title: 비오는 날, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240714/비오는 날/rainy-day.java
+++ 240714/비오는 날/rainy-day.java
@@ -1,56 +1,37 @@
-import java.util.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine(); // 버퍼 비우기
-
-        Rday[] arr = new Rday[n];
-        LocalDate earliestDate = null;
-
-        // 전체 데이터 입력받기
-        for (int i = 0; i < n; i++) {
-            String date = sc.next();
-            String day = sc.next();
-            String weather = sc.next();
-            LocalDate localDate = LocalDate.parse(date);
-            arr[i] = new Rday(localDate, day, weather);
-
-            // 가장 빠른 날짜 찾기
-            if (earliestDate == null || localDate.isBefore(earliestDate)) {
-                earliestDate = localDate;
-            }
-        }
-
-        // 가장 가까운 비 오는 날 찾기
-        Rday closestRainDay = null;
-        for (Rday rday : arr) {
-            if (rday.weather.equals("Rain") && 
-                (closestRainDay == null || rday.date.isBefore(closestRainDay.date)) && 
-                !rday.date.isBefore(earliestDate)) {
-                closestRainDay = rday;
-            }
-        }
-
-        // 출력
-        if (closestRainDay != null) {
-            System.out.println(closestRainDay.date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + 
-                               " " + closestRainDay.day + " " + closestRainDay.weather);
-        }
-    }
-}
-
-class Rday {
-    LocalDate date;
-    String day;
-    String weather;
-    
-    public Rday(LocalDate date, String day, String weather) {
+// Forecast 정보를 나타내는 클래스 선언
+class Forecast {
+    String date, day, weather;
+    Forecast(String date, String day, String weather) {
         this.date = date;
         this.day = day;
         this.weather = weather;
+    }
+};
+
+public class Main {
+    public static Forecast ans = new Forecast("9999-99-99", "", "");
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            String date = sc.next();
+            String day = sc.next();
+            String weather = sc.next();
+
+            // Forecast 객체를 만들어 줍니다.
+            Forecast f = new Forecast(date, day, weather);
+            if(weather.equals("Rain")) {
+                if(ans.date.compareTo(f.date) > 0)
+                    ans = f;
+            }
+        }
+
+        // 결과를 출력합니다.
+        System.out.print(ans.date + " " + ans.day + " " + ans.weather);
     }
 }
