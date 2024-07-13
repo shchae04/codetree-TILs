257184dac393525ed6bdae49fa34cde049aaab5b
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-14 01:20:59 +0900

    Title: 사는 지역, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240714/사는 지역/where-live.java
+++ 240714/사는 지역/where-live.java
@@ -18,12 +18,23 @@ public class Main {
         }
         
         // 이름을 기준으로 정렬
-        Arrays.sort(arr, (a, b) -> b.name.compareTo(a.name));
+        // Arrays.sort(arr, (a, b) -> b.name.compareTo(a.name));
+
+        //정렬 (사전순으로 이름이 가장 느린 사람 찾기)
+        int lastIdx = 0;
+        for(int i=1; i<n; i++){
+            //s1 == s2 -> 0
+            //s1 사전순 뒤에오면 1
+            //s1 사전순 앞에오면 -1
+            if (arr[i].name.compareTo(arr[lastIdx].name) > 0){
+                lastIdx = i;
+            }
+        }
         
         // 정렬 후 첫 번째 원소 (사전순으로 가장 마지막 이름) 출력
-        System.out.println("name " + arr[0].name);
-        System.out.println("addr " + arr[0].pNum);
-        System.out.println("city " + arr[0].locale);
+        System.out.println("name " + arr[lastIdx].name);
+        System.out.println("addr " + arr[lastIdx].pNum);
+        System.out.println("city " + arr[lastIdx].locale);
     }
 }
 
