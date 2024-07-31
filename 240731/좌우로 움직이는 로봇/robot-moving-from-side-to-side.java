f6a1db41d6d45fd80bb36487abaf6dc185bcb784
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-07-31 13:32:25 +0900

    Title: 좌우로 움직이는 로봇, Time: 1087ms, Memory: 58MB, Status: Passed - CodeTree

--- 240731/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240731/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -1,63 +1,74 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_T = 1000000;
+    
+    public static int n, m;
+    public static int[] posA = new int[MAX_T  + 1];
+    public static int[] posB = new int[MAX_T  + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
         
-        int[][] movesA = new int[n][2];
-        int[][] movesB = new int[m][2];
-        
-        for (int i = 0; i < n; i++) {
-            movesA[i][0] = sc.nextInt();
-            movesA[i][1] = sc.next().equals("L") ? -1 : 1;
+        // A가 매 초마다 서있는 위치를 기록
+        int timeA = 1;
+        for(int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            
+            while(t-- > 0) {
+                if(d == 'R')
+                    posA[timeA] = posA[timeA - 1] + 1;
+                else
+                    posA[timeA] = posA[timeA - 1] - 1;
+                
+                timeA++;
+            }
         }
         
-        for (int i = 0; i < m; i++) {
-            movesB[i][0] = sc.nextInt();
-            movesB[i][1] = sc.next().equals("L") ? -1 : 1;
+        // B가 매 초마다 서있는 위치를 기록
+        int timeB = 1;
+        for(int i = 0; i < m; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            
+            while(t-- > 0) {
+                if(d == 'R')
+                    posB[timeB] = posB[timeB - 1] + 1;
+                else
+                    posB[timeB] = posB[timeB - 1] - 1;
+                
+                timeB++;
+            }
         }
         
-        System.out.println(countMeetings(movesA, movesB));
-    }
-    
-    public static int countMeetings(int[][] movesA, int[][] movesB) {
-        int posA = 0, posB = 0;
-        int indexA = 0, indexB = 0;
-        int remainA = 0, remainB = 0;
-        int count = 0;
-        int time = 0;
-        boolean lastDifferent = false;
-        
-        while (indexA < movesA.length || indexB < movesB.length || remainA > 0 || remainB > 0) {
-            time++;
-            
-            if (remainA == 0 && indexA < movesA.length) {
-                remainA = movesA[indexA][0];
-                indexA++;
+        if(timeA < timeB) {
+            for(int i = timeA; i < timeB; i++) {
+                posA[i] = posA[i - 1];
             }
-            if (remainB == 0 && indexB < movesB.length) {
-                remainB = movesB[indexB][0];
-                indexB++;
+        }
+        else if(timeA > timeB) {
+            for(int i = timeB; i < timeA; i++) {
+                posB[i] = posB[i - 1];
             }
-            
-            if (remainA > 0) {
-                posA += movesA[indexA - 1][1];
-                remainA--;
-            }
-            if (remainB > 0) {
-                posB += movesB[indexB - 1][1];
-                remainB--;
-            }
-            
-            if (posA == posB && lastDifferent && time > 1) {
-                count++;
-            }
-            
-            lastDifferent = (posA != posB);
         }
         
-        return count;
+        // 새롭게 만나는 횟수를 구합니다.
+        int cnt = 0;
+        int timeMax = 0;
+        if(timeA < timeB)
+            timeMax = timeB;
+        else
+            timeMax = timeA;
+        
+        for(int i = 1; i < timeMax; i++) {
+            if(posA[i] == posB[i] && posA[i - 1] != posB[i - 1])
+                cnt++;
+        }
+        
+        System.out.print(cnt);
     }
 }
