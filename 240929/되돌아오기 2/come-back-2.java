9ea2effd98c1dcfbf3f75e3ed426241c61cea0bc
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-09-29 19:27:36 +0900

    Title: 되돌아오기 2, Time: 271ms, Memory: 13MB, Status: Passed - CodeTree

--- 240929/되돌아오기 2/come-back-2.java
+++ 240929/되돌아오기 2/come-back-2.java
@@ -1,36 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String commands = scanner.nextLine();
-        scanner.close();
+    public static final int DIR_NUM = 4;
+    
+    public static String dirs;
+    public static int x = 0, y = 0;
+    public static int currDir = 3;
+    
+    // 동, 남, 서, 북 순으로 dx, dy를 정의합니다.
+    public static int[] dx = new int[]{1,  0, -1, 0};
+    public static int[] dy = new int[]{0, -1,  0, 1};
 
-        // 방향을 나타내는 배열: 북, 동, 남, 서
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int directionIndex = 0; // 처음 방향: 북쪽
-        int x = 0, y = 0; // 현재 위치 (0, 0)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        dirs = sc.next();
         
-        for (int time = 0; time < commands.length(); time++) {
-            char command = commands.charAt(time);
+        // flag : 시작점으로 되돌아 왔는지 여부
+        boolean flag = false;
+        
+        // 움직이는 것을 진행합니다.
+        for(int i = 0; i < (int) dirs.length(); i++) {
+            char cDir = dirs.charAt(i);
             
-            if (command == 'L') {
-                directionIndex = (directionIndex + 3) % 4; // 왼쪽으로 90도 회전
-            } else if (command == 'R') {
-                directionIndex = (directionIndex + 1) % 4; // 오른쪽으로 90도 회전
-            } else if (command == 'F') {
-                x += directions[directionIndex][0]; // x 좌표 업데이트
-                y += directions[directionIndex][1]; // y 좌표 업데이트
+            // 반시계방향 90' 회전
+            if(cDir == 'L')
+                currDir = (currDir - 1 + 4) % 4;
+            // 시계방향 90' 회전
+            else if(cDir == 'R')
+                currDir = (currDir + 1) % 4;
+            // 직진
+            else {
+                x += dx[currDir];
+                y += dy[currDir];
             }
             
-            // 현재 위치가 (0, 0)인지 확인
-            if (x == 0 && y == 0) {
-                System.out.println(time + 1); // 1초 단위로 카운트하므로 +1
-                return;
+            // 시작점으로 되돌아왔을 때
+            if(x == 0 && y == 0) {
+                System.out.print(i + 1);
+                flag = true;
+                break;
             }
         }
         
-        // 모든 명령을 실행했지만 (0, 0)으로 돌아오지 못한 경우
-        System.out.println(-1);
+        //시작점으로 되돌아오지 못했을 때
+        if(flag == false)
+            System.out.print(-1);
+        
     }
 }
