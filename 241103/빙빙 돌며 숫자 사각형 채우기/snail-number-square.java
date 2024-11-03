90d73584044cd208bf05dc7b51797456612ae732
shchae04 <94516539+shchae04@users.noreply.github.com>
2024-11-03 23:56:27 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 241103/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
+++ 241103/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -1,53 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_NUM = 100;
+    public static final int DIR_NUM = 4;
+
+    public static int n, m;
+    public static int[][] arr = new int[MAX_NUM][MAX_NUM];
+    
+    public static int[] dx = new int[]{0, 1,  0, -1};
+    public static int[] dy = new int[]{1, 0, -1,  0};
+
+    public static int currX = 0, currY = 0;  // 시작은 (0, 0) 입니다.
+    public static int dir = 0;               // 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽  
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(); // 행의 개수
-        int m = scanner.nextInt(); // 열의 개수
-        
-        int[][] matrix = new int[n][m];
-        int num = 1;
-        int top = 0, bottom = n - 1, left = 0, right = m - 1;
+        Scanner sc = new Scanner(System.in);
 
-        while (top <= bottom && left <= right) {
-            // 왼쪽에서 오른쪽으로 이동
-            for (int i = left; i <= right; i++) {
-                matrix[top][i] = num++;
-            }
-            top++;
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-            // 위에서 아래로 이동
-            for (int i = top; i <= bottom; i++) {
-                matrix[i][right] = num++;
-            }
-            right--;
+        // 처음 시작 위치에 초기값을 적습니다.
+        arr[currX][currY] = 1;
 
-            // 오른쪽에서 왼쪽으로 이동
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    matrix[bottom][i] = num++;
-                }
-                bottom--;
-            }
+        // n*m개의 숫자를 적어야 합니다. 
+        for(int i = 2; i <= n * m; i++) { // 숫자 i를 어디에 적을지 결정합니다.
+            // 현재 방향 dir를 기준으로 그 다음 위치 값을 계산합니다.
+            int nextX = currX + dx[dir], nextY = currY + dy[dir];
 
-            // 아래에서 위로 이동
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    matrix[i][left] = num++;
-                }
-                left++;
-            }
+            // 더 이상 나아갈 수 없다면
+            // 시계방향으로 90'를 회전합니다.
+            if(!inRange(nextX, nextY) || arr[nextX][nextY] != 0)
+                dir = (dir + 1) % 4;
+
+            // 그 다음 위치로 이동한 다음 배열에 올바른 값을 채워넣습니다.
+            currX = currX + dx[dir]; currY = currY + dy[dir];
+            arr[currX][currY] = i;
         }
-
-        // 결과 출력
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
+        
+        // 출력:
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++)
+                System.out.print(arr[i][j] + " ");
             System.out.println();
         }
-
-        scanner.close();
     }
 }
