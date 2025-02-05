03c7ae79faaf35433e55b8b30414b8c94a3fb6a1
shchae04 <94516539+shchae04@users.noreply.github.com>
2025-02-05 22:42:33 +0900

    Title: 씨 오 더블유 2, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250205/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String s = sc.next();
+
+        int countC = 0;
+        int countCO = 0;
+        int countCOW = 0;
+
+        for(int i =0; i<N; i++){
+            char ch = s.charAt(i);
+            if(ch == 'C'){
+                countC++;
+            } else if(ch == 'O'){
+                countCO++;
+            } else if(ch == 'W'){
+                countCOW += countCO;
+            }
+        }
+
+        System.out.println(countCOW);
+    }
+}
